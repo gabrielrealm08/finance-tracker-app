1af30bcbebf7d66faae35c06c52087fe85319ce9
Gabriel Oyibo <gabrieloyibo730gmail.com>
2026-02-06 16:09:41 +0100

    2nd

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/finance-tracker-app/", // ✅ repo name here
+  base: "/finance-tracker-app/",
 });
